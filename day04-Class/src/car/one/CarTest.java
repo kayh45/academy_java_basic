fb576da8b04670e0638596b63fddfa4dc0a9b8c8
kayh45 <kayh4545@gmail.com>
2018-07-09 11:55:53 +0900

    점심 시간 커밋 | 클래스생성 - 메소드생성 및 사용

---
+++ day04-Class/src/car/one/CarTest.java
@@ -0,0 +1,40 @@
+package car.one;
+/**
+ * 
+ * 자동차라는 타입의 실제 객체를 생성하고
+ * 테스트하는 클래스
+ * 
+ * @author PC38219
+ *
+ */
+public class CarTest {
+	public static void main(String[] args) {
+
+		// Car라는 타입의 객체를 사용하기위한 변수가 필요함
+		// 1. 선언
+		Car myCar;
+		
+		// 2. 초기화
+		myCar = new Car();
+		
+		// 3. 사용 : Car 타입 안에 정의된 메소드 호출
+		
+		System.out.println("\n=============================");
+		System.out.println("1. 초기 myCar의 속도");
+		myCar.print();
+		
+		System.out.println("\n=============================");
+		System.out.println("2. 100만큼 가속 후 속도");
+		myCar.accelerate(100);
+		myCar.print();
+
+		System.out.println("\n=============================");
+		System.out.println("3. 정지 후 속도");
+		myCar.stop();
+		myCar.print();
+		
+		
+		
+		
+	}
+}
---
+++ day04-Class/src/practice/methods/MethodsTest.java
@@ -0,0 +1,21 @@
+package practice.methods;
+
+/**
+ * VarietyMethods 클래스를 테스트 하는 클래스
+ * 
+ * @author PC38219
+ *
+ */
+
+public class MethodsTest {
+	public static void main(String[] args) {
+
+		VarietyMethods mMethods;
+		
+		mMethods = new VarietyMethods();
+		
+		mMethods.sayHello();
+		mMethods.maxims("소크라테스", "너 자신을 알라");
+		
+	}
+}
